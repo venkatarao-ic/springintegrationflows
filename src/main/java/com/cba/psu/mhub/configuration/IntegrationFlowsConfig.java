dc4e5365006edfb056f67a5a013c3467c3415dab
Venkatarao.Nersu <venkatarao.nersu@outlook.com>
2021-01-28 18:16:15 +0530

    Spring Integration Flows

---
+++ src/main/java/com/cba/psu/mhub/configuration/IntegrationFlowsConfig.java
@@ -0,0 +1,149 @@
+package com.cba.psu.mhub.configuration;
+
+import com.cba.psu.mhub.model.Payment;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Import;
+import org.springframework.integration.annotation.IntegrationComponentScan;
+import org.springframework.integration.config.EnableIntegration;
+import org.springframework.integration.dsl.IntegrationFlow;
+import org.springframework.integration.dsl.IntegrationFlows;
+import org.springframework.integration.dsl.Pollers;
+import org.springframework.integration.dsl.Transformers;
+import org.springframework.integration.jms.dsl.Jms;
+import org.springframework.integration.kafka.dsl.Kafka;
+import org.springframework.integration.support.json.Jackson2JsonObjectMapper;
+import org.springframework.jms.annotation.JmsListener;
+import org.springframework.kafka.core.ConsumerFactory;
+import org.springframework.kafka.core.KafkaTemplate;
+import org.springframework.kafka.listener.ConsumerProperties;
+
+import javax.jms.ConnectionFactory;
+import java.util.Map;
+
+@Configuration
+@EnableIntegration
+@IntegrationComponentScan
+@Import({KafkaProducingChannelConfig.class})
+public class IntegrationFlowsConfig {
+    private static final Logger logger = LoggerFactory.getLogger(IntegrationFlowsConfig.class);
+
+    private static String SPRING_INTEGRATION_KAFKA_TOPIC = "spring-integration-kafka.t";
+
+    @Value("${mq.queue.payment.in}")
+    private String paymentInQueue;
+
+    @Value("${mq.queue.payment.out}")
+    private String paymentOutQueue;
+
+    @Value("${kafka.topic.payment.in}")
+    private String kafkaPaymentIn;
+
+    @Value("${kafka.topic.payment.out}")
+    private String kafkaPaymentOut;
+
+    @Value("${kafka.topic.payment.in2}")
+    private String kafkaPaymentIn2;
+
+    @Autowired
+    private KafkaTemplate<String, String> kafkaTemplate;
+
+
+    @Autowired
+    @Qualifier("kafkaConsumerFactory")
+    private ConsumerFactory<String, String> consumerFactory;
+
+    @Autowired
+    @Qualifier("kafkaConsumerConfigs")
+    private Map<String, Object> consumerConfigs;
+
+
+    /**
+     * Active MQ to Active MQ through JMS
+     * @param connectionFactory
+     * @return
+     */
+    @Bean
+    public IntegrationFlow flowMQToMQ(ConnectionFactory connectionFactory) {
+        return IntegrationFlows.from(Jms.inboundAdapter(connectionFactory)
+                .destination(paymentInQueue), e -> e.poller(Pollers
+                .fixedDelay(5000)
+                .maxMessagesPerPoll(2)))
+                .transform(Transformers.fromJson(Payment.class, new Jackson2JsonObjectMapper(objectMapper())))
+                .transform(Transformers.toJson())
+                .handle(Jms.outboundAdapter(connectionFactory)
+                        .destination(paymentOutQueue))
+                .get();
+    }
+
+    /**
+     * Flow From KAFKA To KAFKA TOPIC
+     * @param connectionFactory
+     * @return
+     */
+    @Bean
+    public IntegrationFlow flowKafkaToKafka(ConnectionFactory connectionFactory) {
+        return IntegrationFlows
+                .from(Kafka.inboundChannelAdapter(consumerFactory,new ConsumerProperties(kafkaPaymentIn)), e -> e.poller(Pollers
+                        .fixedDelay(5000)
+                        .maxMessagesPerPoll(2)))
+                .transform(Transformers.fromJson(Payment.class, new Jackson2JsonObjectMapper(objectMapper())))
+                .transform(Transformers.toJson())
+                .handle(Kafka.outboundChannelAdapter(kafkaTemplate).topic(kafkaPaymentOut))
+                .get();
+    }
+
+    /**
+     * Flow From Active MQ To KAFKA TOPIC
+     * @param connectionFactory
+     * @return
+     */
+    @Bean
+    public IntegrationFlow flowMQToKafka(ConnectionFactory connectionFactory) {
+        return IntegrationFlows.from(Jms.inboundAdapter(connectionFactory)
+                .destination(paymentInQueue), e -> e.poller(Pollers
+                .fixedDelay(5000)
+                .maxMessagesPerPoll(2)))
+                .channel("integration.payment.jsonToObject.channel")
+                .channel("integration.payment.objectToJson.channel")
+                .handle(Kafka.outboundChannelAdapter(kafkaTemplate).topic(SPRING_INTEGRATION_KAFKA_TOPIC))
+                .get();
+    }
+
+    /**
+     * Flow From KAFKA To MQ
+     * @param connectionFactory
+     * @return
+     */
+    @Bean
+    public IntegrationFlow flowKafkaToMQ(ConnectionFactory connectionFactory) {
+        return IntegrationFlows
+                .from(Kafka.inboundChannelAdapter(consumerFactory,new ConsumerProperties(kafkaPaymentIn2)), e -> e.poller(Pollers
+                        .fixedDelay(5000)
+                        .maxMessagesPerPoll(2)))
+                .transform(Transformers.fromJson(Payment.class, new Jackson2JsonObjectMapper(objectMapper())))
+                .transform(Transformers.toJson())
+                .handle(Jms.outboundAdapter(connectionFactory)
+                        .destination(paymentOutQueue))
+                .get();
+    }
+
+
+
+    @JmsListener(destination = "mq.payment.out")
+    public void listen(String in) {
+        logger.info("Message is being processed " + in);
+    }
+
+    @Bean
+    public ObjectMapper objectMapper(){
+        return new ObjectMapper();
+    }
+
+}
---
+++ src/main/java/com/cba/psu/mhub/controller/ClientController.java
@@ -0,0 +1,99 @@
+package com.cba.psu.mhub.controller;
+
+
+import com.cba.psu.mhub.configuration.IntegrationFlowsConfig;
+import com.cba.psu.mhub.configuration.KafkaProducingChannelConfig;
+import com.cba.psu.mhub.model.Payment;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.apache.kafka.connect.json.JsonConverter;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.Import;
+import org.springframework.integration.dsl.MessageChannels;
+import org.springframework.integration.support.MessageBuilder;
+import org.springframework.jms.core.JmsTemplate;
+import org.springframework.kafka.support.KafkaHeaders;
+import org.springframework.messaging.MessageChannel;
+import org.springframework.messaging.support.GenericMessage;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.io.IOException;
+import java.util.Collections;
+import java.util.Map;
+
+@RestController
+@RequestMapping("/api/v1")
+public class ClientController {
+
+    private static final Logger logger = LoggerFactory.getLogger(ClientController.class);
+
+    @Autowired
+    private JmsTemplate jmsTemplate;
+
+    @Autowired
+    private ObjectMapper objectMapper;
+
+    @Value("${mq.queue.payment.in:mq.payment.in}")
+    private String paymentInQueue;
+
+    @Value("${mq.queue.payment.out:mq.payment.out}")
+    private String paymentOutQueue;
+
+    @Value("${kafka.topic.payment.in:kafka.payment.in}")
+    private String kafkaPaymentIn;
+
+    @Value("${kafka.topic.payment.out:kafka.payment.out}")
+    private String kafkaPaymentOut;
+
+    @Value("${kafka.topic.payment.in2:kafka.payment.in2}")
+    private String kafkaPaymentIn2;
+
+    @Autowired
+    private ApplicationContext context;
+
+    @PostMapping("/queue/payment")
+    public Payment sendMessageToQueue(@RequestBody Payment payment) throws IOException {
+        System.out.println(objectMapper);
+        String message = objectMapper.writeValueAsString(payment);
+        System.out.println(message);
+        jmsTemplate.convertAndSend(paymentInQueue, message);
+        return payment;
+    }
+
+    @PostMapping("/kafka/payment")
+    public Payment sendMessageToKafka(@RequestBody Payment payment) throws IOException {
+        String data = objectMapper.writeValueAsString(payment);
+        System.out.println(data);
+        MessageChannel producingChannel = context.getBean("producingChannel", MessageChannel.class);
+        Map<String, Object> headers = Collections.singletonMap(KafkaHeaders.TOPIC, kafkaPaymentIn);
+        for (int i = 0; i < 10; i++) {
+            GenericMessage<String> message = new GenericMessage<>(data, headers);
+            producingChannel.send(message);
+            logger.info("sent message='{}'", message);
+            return payment;
+        }
+        return payment;
+    }
+
+    @PostMapping("/kafka/payment2")
+    public Payment sendMessageToKafka2(@RequestBody Payment payment) throws IOException {
+        String data = objectMapper.writeValueAsString(payment);
+        System.out.println(data);
+        MessageChannel producingChannel = context.getBean("producingChannel", MessageChannel.class);
+        Map<String, Object> headers = Collections.singletonMap(KafkaHeaders.TOPIC, kafkaPaymentIn2);
+        for (int i = 0; i < 10; i++) {
+            GenericMessage<String> message = new GenericMessage<>(data, headers);
+            producingChannel.send(message);
+            logger.info("sent message='{}'", message);
+            return payment;
+        }
+        return payment;
+    }
+}
